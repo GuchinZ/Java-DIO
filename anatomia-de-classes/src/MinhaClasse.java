5e3e03f41ba2406c702257a35f085d5c83bb47e2
GuchinZ <guschimith33@gmail.com>
2023-04-08 16:41:37 -0300

    Exercícios Java

---
+++ anatomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,12 @@
+public class MinhaClasse {
+    public static void main (String [] args){
+        String primeiroNome = "Gustavo";
+        String segundoNome = "Schimith";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.print(nomeCompleto);
+    }
+    
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
